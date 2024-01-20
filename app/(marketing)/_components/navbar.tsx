de3a833b76fd1d9cb77148c6494ccbc25d86eb2a
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-20 17:04:32 +0530

    add dark mode tailwind classes

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
