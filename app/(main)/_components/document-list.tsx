51c13ccf2d0c0e909de2f6ce6794592708287f26
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-21 19:03:04 +0530

    get documents successfully on sidebar

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,10 +1,26 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
 import { FileIcon } from "lucide-react";
 import { useState } from "react";
 
 const DocumentList = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const { user } = useUser();
+  const documents = useQuery(api.documents.getUserDocuments, {
+    userId: user?.id || "",
+  });
+
+  if (documents === undefined) {
+    return (
+      <div className="items-center justify-center flex pt-10">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
 
   return (
     <div
@@ -14,104 +30,16 @@ const DocumentList = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
-      <div
-        role="button"
-        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-      >
-        <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-        <span className="truncate">Untitled</span>
-      </div>
+      {documents?.map((document) => (
+        <div
+          key={document._id}
+          role="button"
+          className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
+        >
+          <FileIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
+          <span className="truncate">{document.title}</span>
+        </div>
+      ))}
     </div>
   );
 };
