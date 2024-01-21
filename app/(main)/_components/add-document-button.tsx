75d959b5dabb33f4c796df9dacdb1adec984b723
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-21 15:53:29 +0530

    add option in sidebar to create new page

---
+++ app/(main)/_components/add-document-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { PlusCircleIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+const AddDocumentButton = () => {
+  const { user } = useUser();
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    if (!user) return;
+
+    const promise = create({ title: "Untitled", userId: user.id }).then(
+      (documentId) => router.push(`/documents/${documentId}`)
+    );
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
+
+  return (
+    <div
+      onClick={onCreate}
+      role="button"
+      className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+    >
+      <PlusCircleIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
+      <span className="truncate">New Page</span>
+    </div>
+  );
+};
+
+export default AddDocumentButton;
--- app/(main)/_components/sidebar.tsx
+++ app/(main)/_components/sidebar.tsx
@@ -1,9 +1,11 @@
 import UserItem from "./user-menu";
+import AddDocumentButton from "./add-document-button";
 
 const Sidebar = () => {
   return (
     <aside className="w-64 p-4 flex-col hidden md:block bg-background dark:bg-[#303030] h-full">
       <UserItem />
+      <AddDocumentButton />
     </aside>
   );
 };
