112970ec9daf9f05a191ad280d2152b5d18c5eeb
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-21 22:48:24 +0530

    add delete dropdown ui

--- app/(main)/_components/document-item.tsx
+++ app/(main)/_components/document-item.tsx
@@ -1,7 +1,14 @@
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { LucideIcon } from "lucide-react";
-
+import { LucideIcon, MoreHorizontal, Trash } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { useUser } from "@clerk/nextjs";
 interface ItemProps {
   id: Id<"documents">;
   label: string;
@@ -19,12 +26,19 @@ const DocumentItem = ({
   active,
   onClick,
 }: ItemProps) => {
+  const { user } = useUser();
+
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    console.log("Clicked");
+  };
+
   return (
     <div
       onClick={onClick}
       role="button"
       className={cn(
-        "px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md",
+        "group px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md",
         active && "bg-primary/5 text-primary"
       )}
     >
@@ -34,6 +48,31 @@ const DocumentItem = ({
         <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
       )}
       <span className="truncate">{label}</span>
+      <DropdownMenu>
+        <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+          <div
+            role="button"
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <MoreHorizontal className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="w-60"
+          align="start"
+          side="right"
+          forceMount
+        >
+          <DropdownMenuItem onClick={onArchive}>
+            <Trash className="h-4 w-4 mr-2" />
+            Delete
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <div className="text-xs text-muted-foreground p-2">
+            Last edited by: {user?.fullName}
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
