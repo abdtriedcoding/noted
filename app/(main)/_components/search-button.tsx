5c0f3671d04a53dd0be9d575e1f23df5a62e6392
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-22 02:04:51 +0530

    add search commad dialog box

--- app/(main)/_components/search-button.tsx
+++ app/(main)/_components/search-button.tsx
@@ -1,17 +1,70 @@
-import { SearchIcon } from "lucide-react";
+"use client";
+
+import * as React from "react";
+import {
+  CalendarIcon,
+  RocketIcon,
+  ScanFaceIcon,
+  SearchIcon,
+} from "lucide-react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 const SearchButton = () => {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   return (
-    <div
-      role="button"
-      className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
-    >
-      <SearchIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
-      <span className="truncate">Search</span>
-      <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
-        <span className="text-xs">⌘</span>K
-      </kbd>
-    </div>
+    <>
+      <div
+        role="button"
+        onClick={() => setOpen(true)}
+        className="px-4 py-2 text-sm w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium rounded-md"
+      >
+        <SearchIcon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
+        <span className="truncate">Search</span>
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </div>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <ScanFaceIcon className="mr-2 h-4 w-4" />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <RocketIcon className="mr-2 h-4 w-4" />
+              <span>Launch</span>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
 
