b9de591088c02c02fbc85ecc335539e3036eabab
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-21 13:09:58 +0530

    code create document page ui

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Image from "next/image";
+import { currentUser } from "@clerk/nextjs";
+import { PlusCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-const Page = () => {
+const Page = async () => {
+  const user = await currentUser();
+
   return (
-    <div>Page</div>
-  )
-}
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
