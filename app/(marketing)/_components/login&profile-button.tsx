4d8dacc87020ced7dd6ae3628db206b508d9e687
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-21 17:13:21 +0530

    fix loading state🔧

--- app/(marketing)/_components/login&profile-button.tsx
+++ app/(marketing)/_components/login&profile-button.tsx
@@ -1,38 +1,41 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
-import {
-  ClerkLoading,
-  UserButton,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-} from "@clerk/nextjs";
+import { UserButton, SignInButton, useUser, ClerkLoading } from "@clerk/nextjs";
 
 const LoginAndProfileButton = () => {
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <Spinner size={"lg"} />;
+  }
+
   return (
     <>
       <ClerkLoading>
-        <Spinner size={"lg"} />
+        <Spinner size="lg" />
       </ClerkLoading>
-
-      <SignedIn>
-        <Button variant="ghost" size="sm" asChild>
-          <Link href="/documents">Enter Jotion</Link>
-        </Button>
-        <UserButton afterSignOutUrl="/" />
-      </SignedIn>
-
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button variant="ghost" size="sm">
-            Log in
+      {isSignedIn ? (
+        <>
+          <Button variant="ghost" size="sm" asChild>
+            <Link href="/documents">Enter Jotion</Link>
           </Button>
-        </SignInButton>
-        <SignInButton mode="modal">
-          <Button size="sm">Get Jotion free</Button>
-        </SignInButton>
-      </SignedOut>
+          <UserButton afterSignOutUrl="/" />
+        </>
+      ) : (
+        <>
+          <SignInButton mode="modal">
+            <Button variant="ghost" size="sm">
+              Log in
+            </Button>
+          </SignInButton>
+          <SignInButton mode="modal">
+            <Button size="sm">Get Jotion free</Button>
+          </SignInButton>
+        </>
+      )}
     </>
   );
 };
