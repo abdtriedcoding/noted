3ad18826f19c0396605716fe85e324b37c6e6598
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-21 13:41:23 +0530

    add user menu ui dropdown

--- app/(main)/_components/sidebar.tsx
+++ app/(main)/_components/sidebar.tsx
@@ -1,7 +1,9 @@
+import UserItem from "./user-menu";
+
 const Sidebar = () => {
   return (
-    <aside className="w-64 p-4 hidden md:block bg-background dark:bg-[#303030] h-full">
-      Sidebar
+    <aside className="w-64 p-4 flex-col hidden md:block bg-background dark:bg-[#303030] h-full">
+      <UserItem />
     </aside>
   );
 };
