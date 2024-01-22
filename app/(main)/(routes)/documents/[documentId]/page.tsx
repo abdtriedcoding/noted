ddcb147fa94ef8b2aed4e2f5ae58f674c0260084
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-22 13:34:05 +0530

    fetch dynamic document data

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
 
 const Page = () => {
-  return <div>Page</div>;
+  const { user } = useUser();
+  const params = useParams();
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+    userId: user?.id || "",
+  });
+
+  if (document === undefined) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return <div>{document.title}</div>;
 };
 
 export default Page;
