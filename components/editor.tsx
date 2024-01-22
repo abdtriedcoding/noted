7a7bd6ea3532ec1303cfcf568359e18b690816f5
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-23 02:09:00 +0530

    add image upload functionality in rich text editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,11 +1,12 @@
 import { useTheme } from "next-themes";
 import { BlockNoteEditor } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
-import "@blocknote/react/style.css";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
+import "@blocknote/react/style.css";
 
 const Editor = ({
   id,
@@ -15,6 +16,9 @@ const Editor = ({
   initialContent?: string;
 }) => {
   const { user } = useUser();
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
   const update = useMutation(api.documents.update);
 
   const onChange = (content: string) => {
@@ -26,12 +30,20 @@ const Editor = ({
     });
   };
 
-  const { resolvedTheme } = useTheme();
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
+
   const editor: BlockNoteEditor = useBlockNote({
     initialContent: initialContent ? JSON.parse(initialContent) : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
+    uploadFile: handleUpload,
   });
 
   return (
