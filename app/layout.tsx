7100d1bf1449ec98b2b281fb144ca51e82fbd627
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-05-22 11:59:14 +0530

    update: metadata and convex files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
+import { Metadata } from "next";
 import { Toaster } from "sonner";
+import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-import { cn, constructMetadata } from "@/lib/utils";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/providers/theme-provider";
@@ -10,7 +11,66 @@ import { ConvexProvider } from "@/components/providers/convex-provider";
 
 const font = Poppins({ subsets: ["latin"], weight: ["400"] });
 
-export const metadata = constructMetadata();
+export const metadata: Metadata = {
+  title: {
+    default: "Noted",
+    template: `%s - "Noted`,
+  },
+  metadataBase: new URL("https://notedwebapp.vercel.app"),
+  description:
+    "Noted is an all-in-one workspace application that helps you for note-making, project management, collaboration, and more.",
+  keywords: [
+    "Note Making",
+    "Project Management",
+    "Task Management",
+    "Team Collaboration",
+    "Digital Workspace",
+  ],
+  authors: [
+    {
+      name: "abdtriedcoding",
+      url: "https://abdullahsidd.vercel.app",
+    },
+  ],
+  creator: "abdtriedcoding",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://notedwebapp.vercel.app",
+    title: "Noted",
+    description:
+      "Noted is an all-in-one workspace application that helps you for note-making, project management, collaboration, and more.",
+    siteName: "Noted",
+    images: [
+      {
+        url: "/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "Noted",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Noted",
+    description:
+      "Noted is an all-in-one workspace application that helps you for note-making, project management, collaboration, and more.",
+    images: ["/thumbnail.png"],
+    creator: "@abdtriedcoding",
+  },
+  icons: [
+    {
+      media: "(prefers-color-scheme: light)",
+      url: "/logo.svg",
+      href: "/logo.svg",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/logo-dark.svg",
+      href: "/logo-dark.svg",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,65 +1,6 @@
-import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function constructMetadata({
-  title = "Jotion",
-  description = "Jotion is an all-in-one workspace application that helps you for note-making, project management, collaboration, and more.",
-  image = "/thumbnail.png",
-  icons = [
-    {
-      media: "(prefers-color-scheme: light)",
-      url: "/logo.svg",
-      href: "/logo.svg",
-    },
-    {
-      media: "(prefers-color-scheme: dark)",
-      url: "/logo-dark.svg",
-      href: "/logo-dark.svg",
-    },
-  ],
-  noIndex = false,
-}: {
-  title?: string;
-  description?: string;
-  image?: string;
-  icons?: {
-    media: string;
-    url: string;
-    href: string;
-  }[];
-  noIndex?: boolean;
-} = {}): Metadata {
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      images: [
-        {
-          url: image,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [image],
-      creator: "@abdtriedcoding",
-    },
-    icons,
-    metadataBase: new URL("https://jotionweb.vercel.app"),
-    ...(noIndex && {
-      robots: {
-        index: false,
-        follow: false,
-      },
-    }),
-  };
 }
