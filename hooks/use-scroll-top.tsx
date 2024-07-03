cbce2e3298aacd554da1c3d9fc06cc8a6d85bde1
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-07-03 21:19:32 +0530

    fix typo

--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function useScrollTop(threshold = 10) {
+export function useScrollTop(threshold = 10) {
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
