9930d8fbec0d203f111438c2d3e7e284bed4eb2f
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-11-03 19:30:18 +0530

    fix api bug

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,16 +1,9 @@
-import { convexAuthNextjsMiddleware } from '@convex-dev/auth/nextjs/server'
 import { put } from '@vercel/blob'
 import { NextResponse } from 'next/server'
 
 export const runtime = 'edge'
 
-export const POST = convexAuthNextjsMiddleware(async function POST(
-  req,
-  { convexAuth }
-) {
-  if (!convexAuth.isAuthenticated())
-    return new NextResponse('Unauthorized', { status: 401 })
-
+export async function POST(req: Request) {
   if (!process.env.BLOB_READ_WRITE_TOKEN) {
     return new Response(
       "Missing BLOB_READ_WRITE_TOKEN. Don't forget to add that to your .env file.",
@@ -35,4 +28,4 @@ export const POST = convexAuthNextjsMiddleware(async function POST(
   })
 
   return NextResponse.json(blob)
-})
+}
